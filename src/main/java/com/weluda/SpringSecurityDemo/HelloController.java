0ced45c2d0c3d09fe5c18600fb6587f59bb7be88
KaiWelu <kaiweluda@gmail.com>
2025-06-06 13:19:07 +0200

    setup test controller and dependencies

---
+++ src/main/java/com/weluda/SpringSecurityDemo/HelloController.java
@@ -0,0 +1,13 @@
+package com.weluda.SpringSecurityDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet() {
+        return "Hello World!";
+    }
+}
