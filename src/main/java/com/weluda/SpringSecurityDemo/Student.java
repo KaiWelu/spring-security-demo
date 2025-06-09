4e8f2080f1c7ff5756c05bdce442a3d2e87f638e
KaiWelu <kaiweluda@gmail.com>
2025-06-09 23:43:01 +0200

    adds student entity

---
+++ src/main/java/com/weluda/SpringSecurityDemo/Student.java
@@ -0,0 +1,14 @@
+package com.weluda.SpringSecurityDemo;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class Student {
+    private int id;
+    private String name;
+    private int marks;
+}
---
+++ src/main/java/com/weluda/SpringSecurityDemo/StudentController.java
@@ -0,0 +1,8 @@
+package com.weluda.SpringSecurityDemo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+}
